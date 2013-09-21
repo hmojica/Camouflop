814367558502c5c0d91ca3022402343c80cb1b42
Kovak <kovac1066@gmail.com>
2013-09-20 22:23:37 -0600

    added rabbit and hole placeholders, set up some stuff

--- main.py
+++ main.py
@@ -1,26 +1,118 @@
 import kivy
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.widget import Widget
 import kivent_cython
 from kivy.clock import Clock
 from kivy.core.window import Window
+from math import radians, atan2, degrees, pi
 
 class DarkBunnyGame(Widget):
+    rabbit = NumericProperty(None)
+    def __init__(self, **kwargs):
+        super(DarkBunnyGame, self).__init__(**kwargs)
+        Clock.schedule_once(self._init_game)
+
+    def _init_game(self, dt):
+        try: 
+            self.init_game(0)
+        except:
+            print 'failed: rescheduling init'
+            Clock.schedule_once(self._init_game)
+
+    def rabbit_collide_with_hole(self, space, arbiter):
+        gameworld = self.gameworld
+        entities = gameworld.entities
+        rabbit_id = arbiter.shapes[0].body.data
+        hole_id = arbiter.shapes[1].body.data
+        print rabbit_id, hole_id
+        return False
+
+    def on_touch_down(self, touch):
+        rabbit = self.gameworld.entities[self.rabbit]
+        rabbit_position = rabbit['cymunk-physics']['position']
+        XDistance =  (rabbit_position[0]) - touch.x
+        YDistance =  (rabbit_position[1]) - touch.y
+        rotation = atan2(YDistance, XDistance) 
+        body = rabbit['cymunk-physics']['body']
+        body.reset_forces()
+        body.velocity = (0, 0)
+        body.angle = (rotation) - pi
+        unit_vector = body.rotation_vector
+        force_offset = unit_vector[0] * -1 * 32, unit_vector[1] * -1 * 32
+        force = 1000*unit_vector[0], 1000*unit_vector[1]
+        body.apply_force(force, force_offset)
+
+    def add_rabbit(self):
+        x = 100
+        y = 100
+        shape_dict = {'inner_radius': 0, 'outer_radius': 32, 
+        'mass': 50, 'offset': (0, 0)}
+        col_shape = {'shape_type': 'circle', 'elasticity': .5, 
+        'collision_type': 1, 'shape_info': shape_dict, 'friction': 1.0}
+        col_shapes = [col_shape]
+        physics_component = {'main_shape': 'circle', 
+        'velocity': (0, 0), 
+        'position': (x, y), 'angle': 0, 
+        'angular_velocity': 0, 
+        'vel_limit': 250, 
+        'ang_vel_limit': radians(200), 
+        'mass': 50, 'col_shapes': col_shapes}
+        create_component_dict = {'cymunk-physics': physics_component, 
+        'physics_renderer': {'texture': 
+            'rabbit.png', 'size': (64, 64)},}
+        component_order = ['cymunk-physics', 'physics_renderer']
+        entity_id = self.gameworld.init_entity(create_component_dict, component_order)
+        self.rabbit = entity_id
+
+    def add_hole(self):
+        x = 500
+        y = 100
+        shape_dict = {'inner_radius': 0, 'outer_radius': 40, 
+        'mass': 100, 'offset': (0, 0)}
+        col_shape = {'shape_type': 'circle', 'elasticity': .5, 
+        'collision_type': 2, 'shape_info': shape_dict, 'friction': 1.0}
+        col_shapes = [col_shape]
+        physics_component = {'main_shape': 'circle', 
+        'velocity': (0, 0), 
+        'position': (x, y), 'angle': 0, 
+        'angular_velocity': 0, 
+        'vel_limit': 250, 
+        'ang_vel_limit': radians(200), 
+        'mass': 100, 'col_shapes': col_shapes}
+        create_component_dict = {'cymunk-physics': physics_component, 
+        'physics_renderer2': {'texture': 
+            'hole.png', 'size': (80, 80)},}
+        component_order = ['cymunk-physics', 'physics_renderer2']
+        self.gameworld.init_entity(create_component_dict, component_order)
 
     def init_game(self, dt):
         self.setup_states()
+        self.setup_map()
         self.set_state()
+        self.setup_collision_callbacks()
+        self.add_rabbit()
+        self.add_hole()
         Clock.schedule_interval(self.update, 1./60.)
+
+    def setup_map(self):
+        self.gameworld.currentmap = self.gameworld.systems['map']
 
     def update(self, dt):
         self.gameworld.update(dt) 
 
     def setup_states(self):
-        self.gameworld.add_state(state_name='main', systems_added=[], 
+        self.gameworld.add_state(state_name='main', systems_added=[
+            'physics_renderer2', 'physics_renderer'], 
             systems_removed=[], 
             systems_paused=[], systems_unpaused=[],
             screenmanager_screen='main')
+
+    def setup_collision_callbacks(self):
+        systems = self.gameworld.systems
+        physics = systems['cymunk-physics']
+        physics.add_collision_handler(1, 2, 
+            begin_func=self.rabbit_collide_with_hole)
 
     def set_state(self):
         self.gameworld.state = 'main'
@@ -39,7 +131,7 @@ class DebugPanel(Widget):
 class DarkApp(App):
 
     def build(self):
-    	pass
+        pass
 
 if __name__== '__main__':
     DarkApp().run()
