615e1190ca99a288e50ea9e5395c361b915db473
Kovak <kovac1066@gmail.com>
2013-09-21 13:40:30 -0600

    added rabbit animation, animation system

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.core.window import Window
 from math import radians, atan2, degrees, pi
 from functools import partial
 from kivy.vector import Vector
+from kivy.core.image import Image as CoreImage
 
 class RabbitSystem(GameSystem):
     system_id = StringProperty('rabbit_system')
@@ -43,10 +44,16 @@ class RabbitSystem(GameSystem):
         'ang_vel_limit': radians(200), 
         'mass': 50, 'col_shapes': col_shapes}
         rabbit_system = {''}
+        animation_state_dict = {'0': 'assets/white_rabbit/WR1.png', '1': 'assets/white_rabbit/WR2.png',
+        '2': 'assets/white_rabbit/WR3.png', '3': 'assets/white_rabbit/WR4.png','4': 
+        'assets/white_rabbit/WR5.png', '5': 'assets/white_rabbit/WR6.png', 'time_between_frames': .15, 'current_frame': 0,
+        'current_frame_time': 0., 'number_of_frames': 6}
+        animation_system = {'states': {'running': animation_state_dict}, 'current_state': 'running'}
         create_component_dict = {'cymunk-physics': physics_component, 
         'physics_renderer': {'texture': 
-            'rabbit.png', 'size': (64, 64)},}
-        component_order = ['cymunk-physics', 'physics_renderer']
+            'assets/white_rabbit/WR1.png', 'size': (53, 57)},
+        'animation_system': animation_system}
+        component_order = ['cymunk-physics', 'physics_renderer', 'animation_system']
         entity_id = self.gameworld.init_entity(create_component_dict, component_order)
         self.rabbit = entity_id
 
@@ -65,6 +72,55 @@ class RabbitSystem(GameSystem):
             force_offset = unit_vector[0] * -1 * 32, unit_vector[1] * -1 * 32
             force = 1000*unit_vector[0], 1000*unit_vector[1]
             body.apply_force(force, force_offset)
+
+class AnimationSystem(GameSystem):
+    '''
+    Animation component info looks like: {'states': {dict of state_name, state dicts, 'current_state': state_name}
+    state dict looks like: 'number_of_frames': integer, 'frame': 'graphic_str', 
+    'current_frame_time': float, 'time_between_frames': float, 'current_frame': int}
+    '''
+    renderer_to_modify = StringProperty('physics_renderer')
+    system_id = StringProperty('animation_system')
+    
+    def __init__(self, **kwargs):
+        super(AnimationSystem, self).__init__(**kwargs)
+        self.textures = {}
+
+    def load_texture(self, texture_str):
+        textures = self.textures
+        if texture_str not in textures:
+            textures[texture_str] = CoreImage(texture_str).texture
+        texture = textures[texture_str]
+        return texture
+
+    def update(self, dt):
+        gameworld = self.gameworld
+        entities = gameworld.entities
+        system_id = self.system_id
+        rendering_system = self.renderer_to_modify
+        load_texture = self.load_texture
+        for entity_id in self.entity_ids:
+            entity = entities[entity_id]
+            animation_system = entity[system_id]
+            rendering_system = entity[rendering_system]
+            current_state = animation_system['current_state']
+            state_dict = animation_system['states'][current_state]
+            state_dict['current_frame_time'] += dt
+            if state_dict['current_frame_time'] >= state_dict['time_between_frames']:
+                state_dict['current_frame_time'] -= state_dict['time_between_frames']
+                state_dict['current_frame'] += 1
+                if state_dict['current_frame'] >= state_dict['number_of_frames']:
+                    state_dict['current_frame'] = 0
+                texture_str = state_dict[str(state_dict['current_frame'])]
+                rendering_system['quad'].texture = load_texture(texture_str)
+                rendering_system['texture'] = texture_str
+
+
+
+
+
+
+
 
 class DarkBunnyGame(Widget):
     
