199572ce60e0f9cdc3c3aeace711f744fa539a02
Hector Raphael Mojica <hectorraphaelmojica@gmail.com>
2013-09-20 21:05:13 -0600

    Initial blank screen initialization

---
+++ main.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class DarkBunnyGame(Widget):
+    pass
+
+class DarkApp(App):
+
+    def build(self):
+        pass
+
+if __name__== '__main__': DarkApp().run()
